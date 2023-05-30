29b373b71bf3e26c853f84b55e009abc9fbbd230
Pial Kanti Samadder <pialkanti2012@gmail.com>
2023-05-30 17:15:43 +0600

    Removed unused variables and assignment from Experiences components

--- src/app/components/experiences/experiences.component.ts
+++ src/app/components/experiences/experiences.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Experience } from 'src/app/models/Experience';
-import { JobExperience } from 'src/app/models/JobExperience';
 import { ExperiencesService } from 'src/app/services/experiences.service';
 
 @Component({
@@ -9,12 +8,9 @@ import { ExperiencesService } from 'src/app/services/experiences.service';
   styleUrls: ['./experiences.component.css'],
 })
 export class ExperiencesComponent implements OnInit {
-  jobs: JobExperience[] = [];
   experieneces: Experience[] = [];
 
-  constructor(private service: ExperiencesService) {
-    this.jobs = service.getExperiences1();
-  }
+  constructor(private service: ExperiencesService) {}
 
   ngOnInit(): void {
     this.service.getExperiences().subscribe((data) => {
