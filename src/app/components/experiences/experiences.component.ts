ade755cec89f88d8453c036dff41b5797be89aab
Pial Kanti Samadder <pialkanti2012@gmail.com>
2023-06-03 00:20:53 +0600

    Updated experience page and added spinner while loading experiences from server

--- src/app/components/experiences/experiences.component.ts
+++ src/app/components/experiences/experiences.component.ts
@@ -8,12 +8,16 @@ import { ExperiencesService } from 'src/app/services/experiences.service';
   styleUrls: ['./experiences.component.css'],
 })
 export class ExperiencesComponent implements OnInit {
+  loading: boolean = false;
   experieneces: Experience[] = [];
 
-  constructor(private service: ExperiencesService) {}
+  constructor(private service: ExperiencesService) {
+    this.loading = true;
+  }
 
   ngOnInit(): void {
     this.service.getExperiences().subscribe((data) => {
+      this.loading = false;
       this.experieneces = data;
     });
   }
